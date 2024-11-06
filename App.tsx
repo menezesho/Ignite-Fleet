5c8dbd39503593151efe6f89159e2d01d98daecd
Henrique Menezes <82477364+menezesho@users.noreply.github.com>
2024-11-05 23:48:45 -0300

    refactor - organizing app.tsx file

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
 
+import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
 
 import theme from './src/theme';
-import { Loading } from './src/components/Loading';
-import { StatusBar } from 'react-native';
 
 export default function App() {
   const [isFontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
