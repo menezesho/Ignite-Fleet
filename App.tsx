5faaea93a52d214e3da356f642a99246fdce7bb6
Henrique Menezes <82477364+menezesho@users.noreply.github.com>
2024-11-05 23:17:19 -0300

    feat - loading fonts and creating loading component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { SignIn } from './src/screens/SignIn';
 
 import theme from './src/theme';
+import { Loading } from './src/components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  
+  if (!isFontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       <SignIn />
     </ThemeProvider>
   );
